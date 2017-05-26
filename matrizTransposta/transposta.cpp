35673e85bd4ee8995084f188f0a275252a77a3c1
yovanycunha <yovany.cunha@ccc.ufcg.edu.br>
2017-05-26 16:00:56 -0300

    transposta iniciada

---
+++ matrizTransposta/transposta.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+
+  int ordem = 0;
+
+
+  getline(cin, ordem);
+
+  int[ordem][ordem] matriz;
+
+  for (int i = 0; i < ordem; i++) {
+
+    getline(cin, );
+
+  }
+
+
+  return 0;
+}
