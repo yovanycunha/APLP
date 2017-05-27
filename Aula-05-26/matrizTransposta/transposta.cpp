db6ee231fb713a1af216bc0479474edf5ae30aa0
yovanycunha <yovany33@gmail.com>
2017-05-26 21:59:19 -0300

    Primos casados Resolvida

--- Aula-05-26/matrizTransposta/transposta.cpp
+++ Aula-05-26/matrizTransposta/transposta.cpp
@@ -1,23 +1,11 @@
 #include <iostream>
 
-
 using namespace std;
 
 int main() {
 
-  int ordem = 0;
+    
 
 
-  getline(cin, ordem);
-
-  int[ordem][ordem] matriz;
-
-  for (int i = 0; i < ordem; i++) {
-
-    getline(cin, );
-
-  }
-
-
-  return 0;
+    return 0;
 }
